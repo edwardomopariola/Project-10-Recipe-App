b2153355dee2342615eed3ce177a09dce70dddf7
Edward Omopariola <femiedjr@gmail.com>
2024-12-21 01:01:45 -0500

    complete functioning code for recipe app

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-let recipeImage = document.getElementById('recipe-image');
-let recipeName = document.getElementById('recipe-name');
-let recipeInstructions = document.getElementById('recipe-instructions');
-let ingredientList = document.getElementById('ingredient-list');
-let youtubeLink = document.getElementById('youtube-link');
-let recipeBtn = document.getElementById('new-recipe-btn')
-
-window.onload = function () {  //random recipe whne page load
-    document.addEventListener('DOMContentLoaded', () => {
-        getRandomRecipe();
-    
-        document.getElementById('recipeBtn').addEventListener('click', getRandomRecipe);
-    });
-}
+// let recipeImage = document.getElementById('recipe-image');
+// let recipeName = document.getElementById('recipe-name');
+// let recipeInstructions = document.getElementById('recipe-instructions');
+// let ingredientList = document.getElementById('ingredient-list');
+// let youtubeLink = document.getElementById('youtube-link');
+// let recipeBtn = document.getElementById('new-recipe-btn')
 
 function getRandomRecipe() {
     axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
@@ -39,5 +31,32 @@ function getRandomRecipe() {
 
         // clear previous ingredient
         ingredientList.innerHTML = '';
+
+        //using for loop to list the ingredients
+        for (let i = 1; i <= 20; i++) {
+            let ingredient = recipe[`strIngredient${i}`];
+            let measure = recipe[`strMeasure${i}`];
+            if (ingredient) {
+                let listItem = document.createElement('li');
+                listItem.textContent = `${measure} ${ingredient}`;
+                ingredientList.appendChild(listItem);
+            }
+        };
     })
+    .catch(error => {
+        console.error('Error fetching the recipe:', error)
+    });
 }
+
+function setRecipe() {
+    getRandomRecipe();
+}
+
+//fetch random recipe when page load
+document.addEventListener('DOMContentLoaded', () => {
+    setRecipe();
+});
+
+document.getElementById('new-recipe-btn').addEventListener('click', getRandomRecipe);  //new recipe btn
+
+
